04e6fc85e0481e0ed34d12d97256563789cd8d24
zimarquez <ismaelmarquez@utexas.edu>
2023-12-10 19:25:53 -0600

    day 4 part 2 done

--- 4_scratchcards.py
+++ 4_scratchcards.py
@@ -9,6 +9,7 @@ def get_dict(file_path):
     return cards
 
 def print_dict(my_dict):
+    print("")
     for key,val in my_dict.items():
         print(f"{key=}: {val=}")
 
@@ -29,12 +30,54 @@ def all_cards(cards):
     #print_dict(points)
     print(f"{sum=}")
 
+def card_matches(cards):
+    points = {}
+    sum = 0
+    for key,val in cards.items():
+        nums = val.split("|")[0].split()
+        win_nums = val.split("|")[1].split()
+        points[key] = 0
+        for entry in nums:
+            if entry in win_nums:
+                points[key] = points[key] + 1
+    return points
+
+def count_copies(card_wins):
+    print("counting copies")
+    copies = {}
+    max_cards = 0
+    for key in card_wins:
+        copies[int(key)] = 1
+        max_cards = max_cards + 1
+    for key,val in card_wins.items():
+        tmp_key = int(key)
+        tmp_val = copies[int(key)]
+        print(f"{key=}")
+        print(val)
+        print(tmp_val)
+        while val > 0 and tmp_key < max_cards:
+            tmp_key = tmp_key + 1
+            copies[tmp_key] = copies[tmp_key] + tmp_val
+            val = val - 1
+        print_dict(copies)
+    sum = 0
+    for val in copies.values():
+        sum = sum + int(val)
+    print(sum)
 def part1(file_path):
     cards = get_dict(file_path)
     print_dict(cards)
 
     all_cards(cards)
 
+def part2(file_path):
+    cards = get_dict(file_path)
+    print_dict(cards)
+
+    match_dict = card_matches(cards)
+    print_dict(match_dict)
+
+    count_copies(match_dict)
 if __name__ == '__main__':
-    #part1("4_test.txt")
-    part1("4_input.txt")
+    #part2("4_test.txt")
+    part2("4_input.txt")
